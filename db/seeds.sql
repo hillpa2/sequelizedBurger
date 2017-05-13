f46fda9254b99abe04a36ec98b08c01ab25a7d3e
Pat Hill <pathill@Pats-MacBook-Pro.local>
2017-05-12 21:26:30 -0400

    sB

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (name) VALUES ('ABurger');
+INSERT INTO burger (name) VALUES ('BBurger');
+INSERT INTO burger (name) VALUES ('CBurger');
+INSERT INTO burger (name) VALUES ('DBurger');
